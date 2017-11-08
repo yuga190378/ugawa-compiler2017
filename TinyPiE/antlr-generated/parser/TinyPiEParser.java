2246b99eb377bf912b0582c41d49232a78b3c27d
yuga190378 <190378g@ugs.kochi-tech.ac.jp>
2017-11-08 17:12:40 +0900

    演習の問題を解く前の状態で保存

---
+++ TinyPiE/antlr-generated/parser/TinyPiEParser.java
@@ -0,0 +1,376 @@
+// Generated from parser/TinyPiE.g4 by ANTLR 4.6
+package parser;
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class TinyPiEParser extends Parser {
+	static { RuntimeMetaData.checkVersion("4.6", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		T__0=1, T__1=2, ADDOP=3, MULOP=4, IDENTIFIER=5, VALUE=6, WS=7;
+	public static final int
+		RULE_expr = 0, RULE_addExpr = 1, RULE_mulExpr = 2, RULE_unaryExpr = 3;
+	public static final String[] ruleNames = {
+		"expr", "addExpr", "mulExpr", "unaryExpr"
+	};
+
+	private static final String[] _LITERAL_NAMES = {
+		null, "'('", "')'"
+	};
+	private static final String[] _SYMBOLIC_NAMES = {
+		null, null, null, "ADDOP", "MULOP", "IDENTIFIER", "VALUE", "WS"
+	};
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+	@Override
+	public String getGrammarFileName() { return "TinyPiE.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public TinyPiEParser(TokenStream input) {
+		super(input);
+		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+	public static class ExprContext extends ParserRuleContext {
+		public AddExprContext addExpr() {
+			return getRuleContext(AddExprContext.class,0);
+		}
+		public ExprContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_expr; }
+	}
+
+	public final ExprContext expr() throws RecognitionException {
+		ExprContext _localctx = new ExprContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_expr);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(8);
+			addExpr(0);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class AddExprContext extends ParserRuleContext {
+		public MulExprContext mulExpr() {
+			return getRuleContext(MulExprContext.class,0);
+		}
+		public AddExprContext addExpr() {
+			return getRuleContext(AddExprContext.class,0);
+		}
+		public TerminalNode ADDOP() { return getToken(TinyPiEParser.ADDOP, 0); }
+		public AddExprContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_addExpr; }
+	}
+
+	public final AddExprContext addExpr() throws RecognitionException {
+		return addExpr(0);
+	}
+
+	private AddExprContext addExpr(int _p) throws RecognitionException {
+		ParserRuleContext _parentctx = _ctx;
+		int _parentState = getState();
+		AddExprContext _localctx = new AddExprContext(_ctx, _parentState);
+		AddExprContext _prevctx = _localctx;
+		int _startState = 2;
+		enterRecursionRule(_localctx, 2, RULE_addExpr, _p);
+		try {
+			int _alt;
+			enterOuterAlt(_localctx, 1);
+			{
+			{
+			setState(11);
+			mulExpr(0);
+			}
+			_ctx.stop = _input.LT(-1);
+			setState(18);
+			_errHandler.sync(this);
+			_alt = getInterpreter().adaptivePredict(_input,0,_ctx);
+			while ( _alt!=2 && _alt!=org.antlr.v4.runtime.atn.ATN.INVALID_ALT_NUMBER ) {
+				if ( _alt==1 ) {
+					if ( _parseListeners!=null ) triggerExitRuleEvent();
+					_prevctx = _localctx;
+					{
+					{
+					_localctx = new AddExprContext(_parentctx, _parentState);
+					pushNewRecursionContext(_localctx, _startState, RULE_addExpr);
+					setState(13);
+					if (!(precpred(_ctx, 2))) throw new FailedPredicateException(this, "precpred(_ctx, 2)");
+					setState(14);
+					match(ADDOP);
+					setState(15);
+					mulExpr(0);
+					}
+					} 
+				}
+				setState(20);
+				_errHandler.sync(this);
+				_alt = getInterpreter().adaptivePredict(_input,0,_ctx);
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			unrollRecursionContexts(_parentctx);
+		}
+		return _localctx;
+	}
+
+	public static class MulExprContext extends ParserRuleContext {
+		public UnaryExprContext unaryExpr() {
+			return getRuleContext(UnaryExprContext.class,0);
+		}
+		public MulExprContext mulExpr() {
+			return getRuleContext(MulExprContext.class,0);
+		}
+		public TerminalNode MULOP() { return getToken(TinyPiEParser.MULOP, 0); }
+		public MulExprContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_mulExpr; }
+	}
+
+	public final MulExprContext mulExpr() throws RecognitionException {
+		return mulExpr(0);
+	}
+
+	private MulExprContext mulExpr(int _p) throws RecognitionException {
+		ParserRuleContext _parentctx = _ctx;
+		int _parentState = getState();
+		MulExprContext _localctx = new MulExprContext(_ctx, _parentState);
+		MulExprContext _prevctx = _localctx;
+		int _startState = 4;
+		enterRecursionRule(_localctx, 4, RULE_mulExpr, _p);
+		try {
+			int _alt;
+			enterOuterAlt(_localctx, 1);
+			{
+			{
+			setState(22);
+			unaryExpr();
+			}
+			_ctx.stop = _input.LT(-1);
+			setState(29);
+			_errHandler.sync(this);
+			_alt = getInterpreter().adaptivePredict(_input,1,_ctx);
+			while ( _alt!=2 && _alt!=org.antlr.v4.runtime.atn.ATN.INVALID_ALT_NUMBER ) {
+				if ( _alt==1 ) {
+					if ( _parseListeners!=null ) triggerExitRuleEvent();
+					_prevctx = _localctx;
+					{
+					{
+					_localctx = new MulExprContext(_parentctx, _parentState);
+					pushNewRecursionContext(_localctx, _startState, RULE_mulExpr);
+					setState(24);
+					if (!(precpred(_ctx, 2))) throw new FailedPredicateException(this, "precpred(_ctx, 2)");
+					setState(25);
+					match(MULOP);
+					setState(26);
+					unaryExpr();
+					}
+					} 
+				}
+				setState(31);
+				_errHandler.sync(this);
+				_alt = getInterpreter().adaptivePredict(_input,1,_ctx);
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			unrollRecursionContexts(_parentctx);
+		}
+		return _localctx;
+	}
+
+	public static class UnaryExprContext extends ParserRuleContext {
+		public UnaryExprContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_unaryExpr; }
+	 
+		public UnaryExprContext() { }
+		public void copyFrom(UnaryExprContext ctx) {
+			super.copyFrom(ctx);
+		}
+	}
+	public static class VarExprContext extends UnaryExprContext {
+		public TerminalNode IDENTIFIER() { return getToken(TinyPiEParser.IDENTIFIER, 0); }
+		public VarExprContext(UnaryExprContext ctx) { copyFrom(ctx); }
+	}
+	public static class LiteralExprContext extends UnaryExprContext {
+		public TerminalNode VALUE() { return getToken(TinyPiEParser.VALUE, 0); }
+		public LiteralExprContext(UnaryExprContext ctx) { copyFrom(ctx); }
+	}
+	public static class ParenExprContext extends UnaryExprContext {
+		public ExprContext expr() {
+			return getRuleContext(ExprContext.class,0);
+		}
+		public ParenExprContext(UnaryExprContext ctx) { copyFrom(ctx); }
+	}
+
+	public final UnaryExprContext unaryExpr() throws RecognitionException {
+		UnaryExprContext _localctx = new UnaryExprContext(_ctx, getState());
+		enterRule(_localctx, 6, RULE_unaryExpr);
+		try {
+			setState(38);
+			_errHandler.sync(this);
+			switch (_input.LA(1)) {
+			case VALUE:
+				_localctx = new LiteralExprContext(_localctx);
+				enterOuterAlt(_localctx, 1);
+				{
+				setState(32);
+				match(VALUE);
+				}
+				break;
+			case IDENTIFIER:
+				_localctx = new VarExprContext(_localctx);
+				enterOuterAlt(_localctx, 2);
+				{
+				setState(33);
+				match(IDENTIFIER);
+				}
+				break;
+			case T__0:
+				_localctx = new ParenExprContext(_localctx);
+				enterOuterAlt(_localctx, 3);
+				{
+				setState(34);
+				match(T__0);
+				setState(35);
+				expr();
+				setState(36);
+				match(T__1);
+				}
+				break;
+			default:
+				throw new NoViableAltException(this);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public boolean sempred(RuleContext _localctx, int ruleIndex, int predIndex) {
+		switch (ruleIndex) {
+		case 1:
+			return addExpr_sempred((AddExprContext)_localctx, predIndex);
+		case 2:
+			return mulExpr_sempred((MulExprContext)_localctx, predIndex);
+		}
+		return true;
+	}
+	private boolean addExpr_sempred(AddExprContext _localctx, int predIndex) {
+		switch (predIndex) {
+		case 0:
+			return precpred(_ctx, 2);
+		}
+		return true;
+	}
+	private boolean mulExpr_sempred(MulExprContext _localctx, int predIndex) {
+		switch (predIndex) {
+		case 1:
+			return precpred(_ctx, 2);
+		}
+		return true;
+	}
+
+	public static final String _serializedATN =
+		"\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\t+\4\2\t\2\4\3\t"+
+		"\3\4\4\t\4\4\5\t\5\3\2\3\2\3\3\3\3\3\3\3\3\3\3\3\3\7\3\23\n\3\f\3\16\3"+
+		"\26\13\3\3\4\3\4\3\4\3\4\3\4\3\4\7\4\36\n\4\f\4\16\4!\13\4\3\5\3\5\3\5"+
+		"\3\5\3\5\3\5\5\5)\n\5\3\5\2\4\4\6\6\2\4\6\b\2\2*\2\n\3\2\2\2\4\f\3\2\2"+
+		"\2\6\27\3\2\2\2\b(\3\2\2\2\n\13\5\4\3\2\13\3\3\2\2\2\f\r\b\3\1\2\r\16"+
+		"\5\6\4\2\16\24\3\2\2\2\17\20\f\4\2\2\20\21\7\5\2\2\21\23\5\6\4\2\22\17"+
+		"\3\2\2\2\23\26\3\2\2\2\24\22\3\2\2\2\24\25\3\2\2\2\25\5\3\2\2\2\26\24"+
+		"\3\2\2\2\27\30\b\4\1\2\30\31\5\b\5\2\31\37\3\2\2\2\32\33\f\4\2\2\33\34"+
+		"\7\6\2\2\34\36\5\b\5\2\35\32\3\2\2\2\36!\3\2\2\2\37\35\3\2\2\2\37 \3\2"+
+		"\2\2 \7\3\2\2\2!\37\3\2\2\2\")\7\b\2\2#)\7\7\2\2$%\7\3\2\2%&\5\2\2\2&"+
+		"\'\7\4\2\2\')\3\2\2\2(\"\3\2\2\2(#\3\2\2\2($\3\2\2\2)\t\3\2\2\2\5\24\37"+
+		"(";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
